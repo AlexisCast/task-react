b1b958f4103140e317b15004b948232ab7c99f29
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-31 15:25:06 -0700

    add post with API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import TaskList from './Components/TaskList';
 import Profile from './Components/Profile';
 
 import { ButtonT } from './TailwindComponents';
+import Task from './Components/Task';
 
 // import { dummyTasks, dummyUser, dummyHelloWorld } from './__mock__/data.js';
 
@@ -15,6 +16,8 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const [newUser, setNewUser] = useState(null);
+  const [newTask, setNewTask] = useState(null);
 
   useEffect(() => {
     fetchHelloWorld();
@@ -32,7 +35,6 @@ function App() {
       const response = await fetch(`${baseUrl}/`);
       if (!response.ok) {
         const errorMessage = await response.json();
-        console.log(await response);
         throw new Error(
           await `
           Message: ${errorMessage.error}
@@ -64,7 +66,6 @@ function App() {
       });
       if (!response.ok) {
         const errorMessage = await response.json();
-        console.log(await response);
         throw new Error(
           await `
           Message: ${errorMessage.error}
@@ -96,7 +97,6 @@ function App() {
       });
       if (!response.ok) {
         const errorMessage = await response.json();
-        console.log(await response);
         throw new Error(
           await `
           Message: ${errorMessage.error}
@@ -112,9 +112,82 @@ function App() {
     }
   };
 
+  const addUserHandler = async () => {
+    setError(null);
+
+    const newUser = {
+      name: 'Bob Marley',
+      password: '1234567890',
+      age: 21,
+      email: 'userTest@gmail.com'
+    };
+
+    try {
+      const response = await fetch(`${baseUrl}/users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+      });
+      if (!response.ok) {
+        const errorMessage = await response.json();
+        throw new Error(
+          await `
+          Message: ${errorMessage.error || 'Error on Creating User'}
+          Status: ${response.status}
+          URL: ${response.url}`
+        );
+      }
+      const data = await response.json();
+      console.log(data);
+      setNewUser(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const addTaskHandler = async () => {
+    setError(null);
+    // Replace with your actual Bearer token
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGYxMTE4NzJiODY2MWUzNTE5OWRiZTQiLCJpYXQiOjE2OTM1MjAyNjN9.yv37iHhsaxNYQUjXg8IQf5sHxzJb1zgSMlA7qttWSV0';
+    const newTask = {
+      description: 'my new task',
+      completed: false
+    };
+
+    try {
+      const response = await fetch(`${baseUrl}/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(newTask)
+      });
+      console.log(await response);
+      if (!response.ok) {
+        const errorMessage = await response.json();
+        throw new Error(
+          await `
+          Message: ${errorMessage.error || 'Error on Creating User'}
+          Status: ${response.status}
+          URL: ${response.url}`
+        );
+      }
+      const data = await response.json();
+      console.log(data);
+      setNewTask(data);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   const resetHandler = () => {
     setTasks([]);
     setUser(null);
+    setNewUser(null);
   };
 
   const headerTitle = message == null ? <h1>Loading...</h1> : <h1>{message}</h1>;
@@ -131,6 +204,20 @@ function App() {
       </ButtonT>
       <section className="flex flex-wrap py-5">
         <ButtonT
+          className="!bg-[#cca300] hover:!bg-[#806600] mx-2"
+          size="md"
+          variant="primary"
+          onClick={addUserHandler}>
+          Create User
+        </ButtonT>
+        <ButtonT
+          className="!bg-[#cca300] hover:!bg-[#806600] mx-2"
+          size="md"
+          variant="primary"
+          onClick={addTaskHandler}>
+          Create Task
+        </ButtonT>
+        <ButtonT
           className="!bg-[#00cc66] hover:!bg-[#008040] mx-2"
           size="md"
           variant="primary"
@@ -146,6 +233,14 @@ function App() {
         </ButtonT>
       </section>
       <section className='py-5"'>
+        {newUser != null && (
+          <div>
+            <h1>Welcome {newUser.user.name}</h1>
+            <Profile user={newUser.user} />
+          </div>
+        )}
+      </section>
+      <section className='py-5"'>
         {user != null && (
           <div>
             <h1>Profile</h1>
@@ -158,6 +253,14 @@ function App() {
           <div>
             <h1>Tasks</h1>
             <TaskList tasks={tasks} />
+          </div>
+        )}
+      </section>
+      <section className='py-5"'>
+        {newTask != null && (
+          <div>
+            <h1>Tasks Created!</h1>
+            <TaskList tasks={[newTask]} />
           </div>
         )}
       </section>
